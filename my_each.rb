8ded4d4fddb6588bce7d24538fff0560dfd4d7fc
alice T <alicewtien@gmail.com>
2018-09-03 23:13:03 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,7 @@
 def my_each(collection) # put argument(s) here
-  if collection.length > 0
-    i = 0
-    while collection.length > 0
-      yield collection[i]
-      i += 1
-    end
+  index = 0
+  while collection.length > 0
+    yield collection[index]
+    index += 1
   end
 end
